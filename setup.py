260a5d77291c00c7862e00f1bc1f836dc71ba391
Fahri Reza <dozymoe@gmail.com>
2020-11-05 06:35:54 +0700

    quickfix extras_require

--- setup.py
+++ setup.py
@@ -21,10 +21,8 @@ REQUIRED = [
 ]
 
 # What packages are optional?
-EXTRAS = [
-    'celery',
-    'uwsgi-tasks',
-]
+EXTRAS = {
+}
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
